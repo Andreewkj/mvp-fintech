43ff061df8fb29d2b892e0a7af5484e5fe326d98
andreewkj <andreew.k.janurio@outlook.com>
2025-05-03 18:29:06 -0300

    feat: add rabitMQ lib and on docker

--- app/Listeners/NotifyPayee.php
+++ app/Listeners/NotifyPayee.php
@@ -4,9 +4,7 @@ namespace App\Listeners;
 
 use App\Domain\Contracts\Adapters\NotifyAdapterInterface;
 use App\Domain\Contracts\Repositories\UserRepositoryInterface;
-use App\Domain\Entities\User;
 use App\Events\TransferWasCompleted;
-use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
